46058e5fbaf513e106baf2c4eb6feedd1c1f6e9a
Vicenzo Hoffmann <139232852+Hoff08@users.noreply.github.com>
2024-08-21 09:14:10 -0300

    Lógica Booleana e Comparações.js

---
+++ Lógica Booleana e Comparações.js
@@ -0,0 +1,17 @@
+// Declare variáveis x, y e z
+let x = 8;
+let y = 12;
+let z = 8;
+
+// Compare se x é maior ou igual a y e exiba o resultado no console
+console.log(x >= y);
+
+// Verifique se x é diferente de z (usando !=) e se x é estritamente diferente de z (usando !==). Exiba os resultados no console
+console.log(x != z);
+console.log(x !== z);
+
+// Use uma expressão lógica para verificar se x é menor que y e z é maior que x. Exiba o resultado no console
+console.log(x < y && z > x);
+
+// Use uma expressão lógica para verificar se x é igual a z ou y é menor que z. Exiba o resultado no console
+console.log(x === z || y < z);
