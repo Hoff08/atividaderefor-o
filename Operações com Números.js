287b2076a7099c8f9f8e67a91a93fc76deb3d6ee
Vicenzo Hoffmann <139232852+Hoff08@users.noreply.github.com>
2024-08-21 09:13:39 -0300

    Operações com Números.js

---
+++ Operações com Números.js
@@ -0,0 +1,16 @@
+// Declare variáveis num1 e num2
+let num1 = 200.456;
+let num2 = 150.789;
+
+// Arredonde num1 para o valor inteiro mais próximo e exiba no console
+console.log(Math.round(num1));
+
+// Encontre o valor máximo e mínimo entre num1 e num2 usando Math.max e Math.min, respectivamente, e exiba no console
+console.log(Math.max(num1, num2));
+console.log(Math.min(num1, num2));
+
+// Calcule a raiz quadrada de num2 e exiba no console
+console.log(Math.sqrt(num2));
+
+// Converta num1 para uma string com notação científica e exiba no console
+console.log(num1.toExponential());
